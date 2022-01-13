b0bca1345d565363225174722510462124ab05c1
Victor Hugo <victor.hugo.antunes.n@gmail.com>
2022-01-13 11:52:10 -0300

    Solução de problemas na API miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -6,48 +6,48 @@ import { makeServer } from "./server";
 
 makeServer({ environment: "development" });
 
-createServer({
-  models: {
-    transaction: Model,
-  },
+// createServer({
+//   models: {
+//     transaction: Model,
+//   },
 
-  seeds(server) {
-    server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: "Freelance de website",
-          type: "deposit",
-          category: "Desenvolvimento",
-          amount: 6800,
-          createdAt: new Date("2021-02-12 09:00:00"),
-        },
-        {
-          id: 2,
-          title: "Aluguel",
-          type: "withdraw",
-          category: "Casa",
-          amount: 1100,
-          createdAt: new Date("2021-02-14 11:00:00"),
-        },
-      ],
-    });
-  },
+//   seeds(server) {
+//     server.db.loadData({
+//       transactions: [
+//         {
+//           id: 1,
+//           title: "Freelance de website",
+//           type: "deposit",
+//           category: "Desenvolvimento",
+//           amount: 6800,
+//           createdAt: new Date("2021-02-12 09:00:00"),
+//         },
+//         {
+//           id: 2,
+//           title: "Aluguel",
+//           type: "withdraw",
+//           category: "Casa",
+//           amount: 1100,
+//           createdAt: new Date("2021-02-14 11:00:00"),
+//         },
+//       ],
+//     });
+//   },
 
-  routes() {
-    this.namespace = "api";
+//   routes() {
+//     this.namespace = "api";
 
-    this.get("/transactions", () => {
-      return this.schema.all("transaction");
-    });
+//     this.get("/transactions", () => {
+//       return this.schema.all("transaction");
+//     });
 
-    this.post("/transactions", (schema, request) => {
-      const data = JSON.parse(request.requestBody);
+//     this.post("/transactions", (schema, request) => {
+//       const data = JSON.parse(request.requestBody);
 
-      return schema.create("transaction", data);
-    });
-  },
-});
+//       return schema.create("transaction", data);
+//     });
+//   },
+// });
 
 ReactDOM.render(
   <React.StrictMode>
